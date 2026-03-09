1e9398d451f018cd1ab3f699923bd04e863ba29b
Jean Claude <jeanclaude@gordons-Mini.fritz.box>
2026-03-09 12:20:31 +0100

    Fix: Correct basePath for GitHub Pages assets

--- _next/static/aYrdpineo4lMK05SeTF61/_buildManifest.js
+++ _next/static/aYrdpineo4lMK05SeTF61/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ki-starter-set//_next/:path+",
-        "destination": "/ki-starter-set/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
